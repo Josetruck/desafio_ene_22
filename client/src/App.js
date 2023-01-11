6f11305e98cb8d39a9614c98625ef75b7176c527
Josetruck <olemiluis@gmail.com>
2023-01-11 22:14:01 +0100

    login y home

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie"
+
+
+import Login from "./components/Login"
+import Home from './components/Home';
 
 function App() {
+  const [cookies, setCookie, removeCookie] = useCookies(['session']);
+  const [view, setView] = useState("")
+  const [logged, setLogged] = useState("")
+
+
+  useEffect(() => {
+    if (cookies.session) {
+      setLogged(true)
+    }
+  })
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!logged && <Login setLogged={setLogged} />}
+      {view === "" && logged ? <Home /> : null}
     </div>
   );
 }
